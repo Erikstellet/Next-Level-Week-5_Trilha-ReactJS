b7b96c8dc932e12de4559d10b0b69d571fb6224d
Erikstellet <erikstellet@gmail.com>
2021-04-22 01:01:16 -0300

    NLW#5 - Aula 1 Part 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,36 @@
-import { Header } from "../components/Header";
+// import { useEffect } from "react";
 
-export default function Home()
+export default function Home(props)
 {
+  // SPA
+  /*
+    useEffect(() => 
+    {
+      fetch('http://localhost:3333/episodes').then(res => res.json())
+                                            .then(data => console.log(data));
+    }, [])
+  */
+
   return (
     <h1>Index</h1>
   )
 }
+
+// SSR getServerSideProps() -> props
+// SSG getStaticProps() -> props -> revalidate -> *Só funciona em produção.
+
+export async function getStaticProps()
+{
+  const res = await fetch('http://localhost:3333/episodes');
+  const data = await res.json();
+
+  return {
+    props: 
+    {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
+
+
